5fe4c8438f2c06272c1334df3ddd3988d76b5167
nast <skorodinskaya01@gmail.com>
2021-08-11 10:19:23 +0300

    Some changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <div>
       <User id={'1'} name={'Leanne Graham'} username={'Bret'} email={'Sincere@april.biz'}/>
+      <User id={'2'} name={'Ervin Howell'} username={'Antonette'} email={'Shanna@melissa.tv'}/>
+      <User id={'3'} name={'Clementine Bauch'} username={'Samantha'} email={'Nathan@yesenia.net'}/>
+      <User id={'4'} name={'Patricia Lebsack'} username={'Karianne'} email={'Julianne.OConner@kory.org'}/>
+      <User id={'5'} name={'Chelsey Dietrich'} username={'Kamren'} email={'Lucio_Hettinger@annie.ca'}/>
+      <User id={'6'} name={'Mrs. Dennis Schulist'} username={'Leopoldo_Corkery'} email={'Karley_Dach@jasper.info'}/>
+      <User id={'7'} name={'Kurtis Weissnat'} username={'Elwyn.Skile'} email={'Telly.Hoeger@billy.biz'}/>
+      <User id={'8'} name={'Nicholas Runolfsdottir V'} username={'Maxime_Nienow'} email={'Sherwood@rosamond.meo'}/>
+      <User id={'9'} name={'Glenna Reicher'} username={'Delphine'} email={'Chaim_McDermott@dana.io'}/>
+      <User id={'10'} name={'Clementina DuBuqu'} username={'Moriah.Stanto'} email={'Rey.Padberg@karina.biz'}/>
     </div>
   );
 }
